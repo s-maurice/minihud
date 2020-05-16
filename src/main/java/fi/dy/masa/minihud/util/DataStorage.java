8b1e620bb9cb429f48f7cbb9824159da8a6f053e
s-maurice <mauricemscholtes@gmail.com>
2020-05-16 20:28:33 +0200

    in testing state, converted to use ClientStructureGeneration

--- src/main/java/fi/dy/masa/minihud/util/DataStorage.java
+++ src/main/java/fi/dy/masa/minihud/util/DataStorage.java
@@ -443,7 +443,7 @@ public class DataStorage
     }
 
     private void updateStructureDataFromClientGeneration(BlockPos playerPos) {
-        if (this.worldSeedValid) {
+        if (this.worldSeedValid && this.mc.world != null) {
             final DimensionType dimensionType = this.mc.player.dimension;
 
             List<StructureType> enabledTypes = new ArrayList<>();
@@ -454,12 +454,9 @@ public class DataStorage
                 }
             }
 
-            if (enabledTypes.isEmpty() == false) {
+            if (!enabledTypes.isEmpty()) {
 
-                // using structure generation check from cubiomes' getStructurePos()
-
-                final int maxChunkRange = this.mc.options.viewDistance;
-//                final int maxChunkRange = this.mc.options.viewDistance + 2;
+                final int maxChunkRange = this.mc.options.viewDistance; // +2?
 
                 // shift 4 to get chunk, shift 5 to get region - only gets one structure per region
                 int minRX = ((playerPos.getX() >> 4) - maxChunkRange) >> 5;
@@ -467,81 +464,21 @@ public class DataStorage
                 int maxRX = ((playerPos.getX() >> 4) + maxChunkRange) >> 5;
                 int maxRZ = ((playerPos.getZ() >> 4) + maxChunkRange) >> 5;
 
-
                 for (int rz = minRZ; rz <= maxRZ; ++rz) {
                     for (int rx = minRX; rx <= maxRX; ++rx) {
-                        System.out.println(String.format("%d, %d", rx, rz));
+                        for (StructureType type : enabledTypes) {
+                            System.out.println(String.format("%d, %d", rx, rz));
 
-                        long seed = this.getWorldSeed(dimensionType);
+                            StructureData structureData = ClientStructureGeneration.getStructurePos(this.mc.world,
+                                                                                                    type,
+                                                                                                    rx,
+                                                                                                    rz,
+                                                                                                    this.mc.player.getBlockPos(),
+                                                                                                    maxChunkRange);
 
-                        seed = rx*341873128712L + rz*132897987541L + seed + 14357620; // testing with witch hut temporarily
-//                      seed = rz*341873128712L + rz*132897987541L + seed + config.seed;
-
-                        seed = (seed ^ 25214903917L);// & ((1LL << 48) - 1);
-
-                        seed = (seed * 25214903917L + 11) & 281474976710655L;
-
-                        int posX, posZ;
-
-//                      if ((config.chunkrange & (config.chunkrange-1)) == 0)
-                        if ((24 & (24 - 1)) != 0) {
-                            // not power of 2
-//                          posX = (int) (seed >> 17) % config.chunkRange;
-                            posX = (int) (seed >> 17) % 24;
-
-                            seed = (seed * 25214903917L + 11) & 281474976710655L;
-//                          posZ = (int) (seed >> 17) % config.chunkRange;
-                            posZ = (int) (seed >> 17) % 24;
-                        } else {
-                            // power of 2
-
-//                          posX = (config.chunkRange * (seed >> 17)) >> 31;
-                            posX = (int) (24 * (seed >> 17)) >> 31;
-
-                            seed = (seed * 25214903917L + 11) & 281474976710655L;
-//                          posZ = (config.chunkRange * (seed >> 17)) >> 31;
-                            posZ = (int) (24 * (seed >> 17)) >> 31;
-                        }
-
-                        // offset pos to (9, 9) in chunk
-                        // biome check is performed at (9, 9) in chunk. but structure origin is at (0, 0)
-//                      posX = ((rx*config.regionSize + posX) << 4) + 9;
-//                      posZ = ((rz*config.regionSize + posZ) << 4) + 9;
-                        posX = ((rx*32 + posX) << 4) + 9;
-                        posZ = ((rz*32 + posZ) << 4) + 9;
-
-//                      create feature
-                        ChunkRandom rotationChunkRandom = new ChunkRandom();
-                        rotationChunkRandom.setStructureSeed(this.getWorldSeed(dimensionType), posX >> 4, posZ >> 4);
-
-                        StructurePieceWithDimensions swampHutGenerator = new SwampHutGenerator(rotationChunkRandom, posX-9, posZ-9);
-                        BlockBox witchBB = swampHutGenerator.getBoundingBox();
-
-                        // correct for witch hut height bug
-                        witchBB.minY -= 1;
-                        witchBB.maxY -= 1;
-
-                        if (MiscUtils.isStructureWithinRange(witchBB, playerPos, maxChunkRange << 4)){
-//                         using actual biome to check is structure pos is valid
-//                         greatly reduces complexity, as well as acting as a minimal check for validity of generated structure
-
-                            Biome biome = mc.world.getBiome(new BlockPos(posX, 100, posZ));
-                            if (biome != null) {
-                                String message = String.format("pos found: %d %d %s", posX-9, posZ-9, biome.toString());
-                                InfoUtils.showInGameMessage(Message.MessageType.SUCCESS, 1000, message);
-                                System.out.println(message);
-
-                                // create and add structure
-                                StructureStart structureStart = new SwampHutFeature.Start(StructureFeature.SWAMP_HUT, posX>>4, posZ>>4, witchBB, 0, 14357620L);
-                                StructureData structureData = StructureData.fromStructureStart(StructureType.WITCH_HUT, structureStart);
-                                this.structures.put(StructureType.OCEAN_MONUMENT, structureData); // dif colour for debug
-
-                                System.out.println("created, put");
+                            if (structureData != null) {
+                                this.structures.put(type, structureData);
                             }
-                        }
-                        else {
-                            String outrngmsg = String.format("out range %d %d", posX, posZ);
-                            System.out.println(outrngmsg);
                         }
                     }
                 }
